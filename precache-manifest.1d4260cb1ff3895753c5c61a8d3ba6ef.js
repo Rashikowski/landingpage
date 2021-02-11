276f6869a094a60f13ca8449ebf68c64feb285af
Rashikowski <67384377+Rashikowski@users.noreply.github.com>
2021-02-11 13:35:33 +0100

    Updates

---
+++ precache-manifest.1d4260cb1ff3895753c5c61a8d3ba6ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d8c564d8873f3b8c5402c9043502aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee8223a6802675dfb347",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1def542df3adcc3e1d37",
+    "url": "/static/js/2.ef9b312c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ef9b312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8223a6802675dfb347",
+    "url": "/static/js/main.327ce102.chunk.js"
+  },
+  {
+    "revision": "e22fd322f0b45aec7fb4",
+    "url": "/static/js/runtime-main.a7f6340d.js"
+  }
+]);
